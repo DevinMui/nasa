130744465d59c779fcbc52e34d148b019bfc371f
Devin <devinmui@yahoo.com>
2016-04-24 02:22:31 -0700

    alexa should work

--- alexa/index.js
+++ alexa/index.js
@@ -1,6 +1,7 @@
 /**
  * App ID for the skill
  */
+var request = require('request')
 var APP_ID = "amzn1.echo-sdk-ams.app.9db0bf66-bb7d-48f3-8a98-17e67d7fc0be"; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
 
 /**
@@ -9,42 +10,53 @@ var APP_ID = "amzn1.echo-sdk-ams.app.9db0bf66-bb7d-48f3-8a98-17e67d7fc0be"; //re
 var AlexaSkill = require('./AlexaSkill');
 
 /**
- * HelloWorld is a child of AlexaSkill.
+ * Hello is a child of AlexaSkill.
  * To read more about inheritance in JavaScript, see the link below.
  *
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
  */
-var HelloWorld = function () {
+var Hello = function () {
     AlexaSkill.call(this, APP_ID);
 };
 
 // Extend AlexaSkill
-HelloWorld.prototype = Object.create(AlexaSkill.prototype);
-HelloWorld.prototype.constructor = HelloWorld;
+Hello.prototype = Object.create(AlexaSkill.prototype);
+Hello.prototype.constructor = Hello;
 
-HelloWorld.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
-    console.log("HelloWorld onSessionStarted requestId: " + sessionStartedRequest.requestId
+Hello.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+    console.log("Hello onSessionStarted requestId: " + sessionStartedRequest.requestId
         + ", sessionId: " + session.sessionId);
     // any initialization logic goes here
 };
 
-HelloWorld.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-    console.log("HelloWorld onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+Hello.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    console.log("Hello onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
     var speechOutput = "Welcome to the Alexa Skills Kit, you can say hello";
     var repromptText = "You can say hello";
     response.ask(speechOutput, repromptText);
 };
 
-HelloWorld.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
-    console.log("HelloWorld onSessionEnded requestId: " + sessionEndedRequest.requestId
+Hello.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+    console.log("Hello onSessionEnded requestId: " + sessionEndedRequest.requestId
         + ", sessionId: " + session.sessionId);
     // any cleanup logic goes here
 };
 
-HelloWorld.prototype.intentHandlers = {
+Hello.prototype.intentHandlers = {
     // register custom intent handlers
     "FlyDrone": function (intent, session, response) {
-        response.tellWithCard("Let's the drone", "Flying drone", "Let's get lot of water data");
+        response.intent.latitude, response.intent.longitude
+        var options = {
+            url: 'https://api.github.com/repos/request/request',
+            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+            body: JSON.stringify({ "long": response.intent.longitude, "lat": response.intent.latitude })
+        };
+        request.post(options, function(err, res, body){
+            if(!err)
+                response.tellWithCard("Let's fly the drone", "Flying drone", "Let's get lot of water data");
+            else
+                response.tellWithCard("Oh no. We could not reach the server")
+        })
     },
     "AMAZON.HelpIntent": function (intent, session, response) {
         response.ask("You can say hello to me!", "You can say hello to me!");
@@ -53,7 +65,7 @@ HelloWorld.prototype.intentHandlers = {
 
 // Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
-    // Create an instance of the HelloWorld skill.
-    var helloWorld = new HelloWorld();
-    helloWorld.execute(event, context);
+    // Create an instance of the Hello skill.
+    var skill = new Hello();
+    skill.execute(event, context);
 };
--- server/index.js
+++ server/index.js
@@ -16,7 +16,8 @@ var flightSchema = new mongoose.Schema({
 	long: Number,
 	lat: Number,
 	reached: Boolean,
-	pictures: Array
+	pictures: Array,
+	soilMosture: Number
 })
 
 var Flight = mongoose.model('Flight', flightSchema)
@@ -44,9 +45,21 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 // routes
 
+app.get('/latest', function(req, res){
+	Flight.findOne({}, {}, { sort: { 'created_at': -1 } }, function(err, doc){ // gets latest one
+		if(!err)
+			res.send(doc)
+		else
+			res.send(err)
+	})
+})
+
 app.get('/flight', function(req, res){
 	Flight.findOne({ _id: req.body.id }, function(err, doc){
-		res.send(doc)
+		if(!err)
+			res.send(doc)
+		else
+			res.send(err)
 	})
 })
 
@@ -55,7 +68,8 @@ app.post('/flight', function(req, res){
 		long: req.body.long,
 		lat: req.body.lat,
 		reached: false,
-		pictures: []
+		pictures: [],
+		soilMosture: -1
 	})
 	flight.save(function(err, flight){
 		if(err)
@@ -68,6 +82,7 @@ app.post('/flight', function(req, res){
 app.post('/reached', function(req, res){
 	Flight.findOne({ _id: req.body.id }, function(err, doc){
 		doc.reached = true
+		doc.soilMosture = req.body.soilMosture
 		doc.save()
 	})
 })
@@ -86,3 +101,11 @@ app.post('/pictures', upload.array('upload', 20), function(req, res){
 app.listen(3000, function () {
   console.log('running on port 3000');
 });
+
+/*
+var latest = # http request
+while true
+	# http request
+	if latest != request.latest
+		# set waypoint
+*/
