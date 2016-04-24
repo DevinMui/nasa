2a141a10a2ceb455fc5d64b7ebdb35581b2084d4
Jesse Liang <jesseliang@ymail.com>
2016-04-24 07:44:20 -0700

    takeoff

---
+++ ObjcSampleCode/DJISdkDemo/Startup/DJIRootViewController.m
@@ -0,0 +1,217 @@
+//
+//  DJIRootViewController.m
+//  DJISdkDemo
+//
+//  Copyright (c) 2015 DJI. All rights reserved.
+//
+
+#import "DJIRootViewController.h"
+#import "ComponentSelectionViewController.h"
+#import "DemoAlertView.h"
+#import "DemoUtilityMacro.h"
+#import "AFNetworking.h"
+#import "FCGeneralControlViewController.h"
+#import "DemoComponentHelper.h"
+#import "DemoAlertView.h"
+#import <DJISDK/DJISDK.h>
+#define ENTER_DEBUG_MODE 0
+#define ENABLE_REMOTE_LOGGER 0
+
+@interface DJIRootViewController ()
+
+- (IBAction)takeOffBtn:(id)sender;
+
+@property(nonatomic, weak) DJIBaseProduct* product;
+@property (weak, nonatomic) IBOutlet UILabel *productConnectionStatus;
+@property (weak, nonatomic) IBOutlet UILabel *productModel;
+@property (weak, nonatomic) IBOutlet UILabel *productFirmwarePackageVersion;
+@property (weak, nonatomic) IBOutlet UIButton *connectButton;
+@property (weak, nonatomic) IBOutlet UILabel *sdkVersionLabel;
+@end
+
+@implementation DJIRootViewController
+
+NSData *latest;
+
+- (void)startTimedTask
+{
+    
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    NSString *URLString = @"http://nasa.devinmui.xyz/latest";
+    [manager GET:URLString parameters:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
+        NSLog(@"JSON: %@", responseObject);
+        latest = responseObject;
+    } failure:^(NSURLSessionTask *operation, NSError *error) {
+        NSLog(@"Error: %@", error);
+        latest = error;
+    }];
+    NSTimer *fiveSecondTimer = [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(performBackgroundTask) userInfo:nil repeats:YES];
+}
+
+- (void)performBackgroundTask
+{
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSString *URLString = @"http://nasa.devinmui.xyz/latest";
+        //NSDictionary *parameters = @{@"foo": @"bar", @"baz": @[@1, @2, @3]};
+        
+        //[[AFHTTPRequestSerializer serializer] requestWithMethod:@"GET" URLString:URLString parameters:nil error:nil];
+        
+        AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+        [manager GET:URLString parameters:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
+            NSLog(@"JSON: %@", responseObject);
+            if(responseObject == latest){
+                // fly the drone
+                // get responseObject.long, responseObject.lat
+            }
+        } failure:^(NSURLSessionTask *operation, NSError *error) {
+            NSLog(@"Error: %@", error);
+        }];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            //Update UI
+        });
+    });
+}
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self startTimedTask];
+    
+    //Register App with App Key
+    NSString* appKey = @"d8d5e0bbff124f85dce2be5c"; //TODO: Please enter your App Key here
+    
+    if ([appKey length] == 0) {
+        ShowResult(@"Please enter your app key.");
+    }
+    else
+    {
+        [DJISDKManager registerApp:appKey withDelegate:self];
+    }
+    
+    [self initUI];
+}
+- (IBAction)takeOffBtn:(id)sender {
+    DJIFlightController* fc = [DemoComponentHelper fetchFlightController];
+    if (fc) {
+        [fc takeoffWithCompletion:^(NSError * _Nullable error) {
+            if (error) {
+                ShowResult(@"Takeoff Error:%@", error.localizedDescription);
+            }
+            else
+            {
+                ShowResult(@"Takeoff Succeeded.");
+            }
+        }];
+    }
+    else
+    {
+        ShowResult(@"Component Not Exist");
+    }
+
+    
+}
+
+- (void)initUI
+{
+    self.title = @"DJI iOS SDK Sample";
+    self.sdkVersionLabel.text = [@"DJI SDK Version: " stringByAppendingString:[DJISDKManager getSDKVersion]];
+    self.productFirmwarePackageVersion.hidden = YES;
+    self.productModel.hidden = YES;
+    //Disable the connect button by default
+    [self.connectButton setEnabled:NO];
+}
+
+- (void)viewDidAppear:(BOOL)animated{
+    if(self.product){
+        [self updateStatusBasedOn:self.product];
+    }
+}
+
+-(IBAction) onConnectButtonClicked:(id)sender
+{
+    if (self.product) {
+        ComponentSelectionViewController* inspireVC = [[ComponentSelectionViewController alloc] init];
+        [self.navigationController pushViewController:inspireVC animated:YES];
+    }
+}
+
+#pragma mark -
+-(void) sdkManagerDidRegisterAppWithError:(NSError *)error {
+    if (error) {
+        ShowResult(@"Registration Error:%@", error);
+        [self.connectButton setEnabled:NO];
+    }
+    else {
+        
+#if ENTER_DEBUG_MODE
+        [DJISDKManager enterDebugModeWithDebugId:@"Enter Debug ID Here"];
+#else
+        [DJISDKManager startConnectionToProduct];
+#endif
+        
+#if ENABLE_REMOTE_LOGGER
+        [DJISDKManager enableRemoteLoggingWithDeviceID:@"Device ID" logServerURLString:@"Enter Remote Logger URL here"];
+#endif
+    }
+    
+}
+
+-(void) sdkManagerProductDidChangeFrom:(DJIBaseProduct* _Nullable) oldProduct to:(DJIBaseProduct* _Nullable) newProduct{
+    if (newProduct) {
+        self.product = newProduct;
+        [self.connectButton setEnabled:YES];
+        
+    } else {
+        NSString* message = [NSString stringWithFormat:@"Connection lost. Back to root. "];
+        UIAlertView* alertView = [[UIAlertView alloc] initWithTitle:@"" message:message delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Back", nil];
+        [self.connectButton setEnabled:NO];
+        
+        [alertView show];
+        self.product = nil;
+    }
+    
+    [self updateStatusBasedOn:newProduct];
+}
+
+#pragma mark -
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex == 1) {
+        if (![self.navigationController.topViewController isKindOfClass:[DJIRootViewController class]]) {
+            [self.navigationController popToRootViewControllerAnimated:NO];
+        }
+    }
+}
+
+-(void) updateFirmwareVersion:(NSString*) version {
+    if (nil != version) {
+        _productFirmwarePackageVersion.text = [NSString stringWithFormat:NSLocalizedString(@"Firmware Package Version: \%@", @""),version];
+        self.productFirmwarePackageVersion.hidden = NO;
+    } else {
+        _productFirmwarePackageVersion.text = NSLocalizedString(@"Firmware Package Version: Unknown", @"");
+        self.productFirmwarePackageVersion.hidden = YES;
+    }
+}
+
+-(void) updateStatusBasedOn:(DJIBaseProduct* )newConnectedProduct {
+    if (newConnectedProduct){
+        _productConnectionStatus.text = NSLocalizedString(@"Status: Product Connected", @"");
+        _productModel.text = [NSString stringWithFormat:NSLocalizedString(@"Model: \%@", @""),newConnectedProduct.model];
+        _productModel.hidden = NO;
+        WeakRef(target);
+        [newConnectedProduct getFirmwarePackageVersionWithCompletion:^(NSString * _Nonnull version, NSError * _Nullable error) {
+            WeakReturn(target);
+            if (error == nil) {
+                [target updateFirmwareVersion:version];
+            }else {
+                [target updateFirmwareVersion:nil];
+            }
+        }];
+    }else {
+        _productConnectionStatus.text = NSLocalizedString(@"Status: Product Not Connected", @"");
+        _productModel.text = NSLocalizedString(@"Model: Unknown", @"");
+        [self updateFirmwareVersion:nil];
+    }
+}
+@end
