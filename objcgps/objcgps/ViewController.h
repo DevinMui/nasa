699be2168398324072e1cc14963e49134c5ef40d
Jesse Liang <jesseliang@ymail.com>
2016-04-24 13:38:03 -0700

    gps that might or might not work

--- objcgps/objcgps/ViewController.h
+++ objcgps/objcgps/ViewController.h
@@ -7,58 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 #import "AFNetworking.h"
 
 @interface ViewController : UIViewController
 
-
-@end
-@implementation ViewController
-
-NSData *latest;
-
-- (void)startTimedTask
-{
-    
-    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
-    NSString *URLString = @"http://nasa.devinmui.xyz/latest";
-    [manager GET:URLString parameters:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
-        NSLog(@"JSON: %@", responseObject);
-        latest = responseObject;
-    } failure:^(NSURLSessionTask *operation, NSError *error) {
-        NSLog(@"Error: %@", error);
-        latest = error;
-    }];
-    NSTimer *fiveSecondTimer = [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(performBackgroundTask) userInfo:nil repeats:YES];
-}
-
-- (void)performBackgroundTask
-{
-    
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        NSString *URLString = @"http://nasa.devinmui.xyz/latest";
-        //NSDictionary *parameters = @{@"foo": @"bar", @"baz": @[@1, @2, @3]};
-        
-        //[[AFHTTPRequestSerializer serializer] requestWithMethod:@"GET" URLString:URLString parameters:nil error:nil];
-        
-        AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
-        [manager GET:URLString parameters:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
-            NSLog(@"JSON: %@", responseObject);
-            if(responseObject == latest){
-                // fly the drone
-                // get responseObject.long, responseObject.lat
-            }
-        } failure:^(NSURLSessionTask *operation, NSError *error) {
-            NSLog(@"Error: %@", error);
-        }];
-        
-        dispatch_async(dispatch_get_main_queue(), ^{
-            //Update UI
-        });
-    });
-}
-
-
+@property (nonatomic,retain) CLLocationManager *locationManager;
 
 @end
 
--- objcgps/objcgps/ViewController.m
+++ objcgps/objcgps/ViewController.m
@@ -6,6 +6,8 @@
 //  Copyright © 2016 Jesse Liang. All rights reserved.
 //
 
+#import "AFNetworking.h"
+#import <CoreLocation/CoreLocation.h>
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -14,14 +16,74 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+NSData *latest;
+CLLocationManager *locationManager;
+NSString *latitudeData;
+NSString *longitudeData;
+
+- (void)startTimedTask
+{
+    
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    NSString *URLString = @"http://nasa.devinmui.xyz/latest";
+    [manager GET:URLString parameters:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
+        NSLog(@"JSON: %@", responseObject);
+        latest = responseObject;
+    } failure:^(NSURLSessionTask *operation, NSError *error) {
+        NSLog(@"Error: %@", error);
+        latest = error;
+    }];
+    NSTimer *fiveSecondTimer = [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(performBackgroundTask) userInfo:nil repeats:YES];
+}
+
+- (void)performBackgroundTask
+{
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSString *URLString = @"http://nasa.devinmui.xyz/latest";
+        
+        
+        //NSDictionary *parameters = @{@"foo": @"bar", @"baz": @[@1, @2, @3]};
+        
+        //[[AFHTTPRequestSerializer serializer] requestWithMethod:@"GET" URLString:URLString parameters:nil error:nil];
+        
+        AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+        [manager GET:URLString parameters:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
+            NSLog(@"JSON: %@", responseObject);
+            if(responseObject == latest){
+                // fly the drone
+                // get responseObject.long, responseObject.lat
+            }
+        } failure:^(NSURLSessionTask *operation, NSError *error) {
+            NSLog(@"Error: %@", error);
+        }];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSLog(longitudeData);
+        });
+    });
+}
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    locationManager = [[CLLocationManager alloc]init]; // initializing locationManager
+    locationManager.delegate = self; // we set the delegate of locationManager to self.
+    locationManager.desiredAccuracy = kCLLocationAccuracyBest; // setting the accuracy
+    
+    [locationManager startUpdatingLocation];  //requesting location updates
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error{
+    UIAlertView *errorAlert = [[UIAlertView alloc]initWithTitle:@"Error" message:@"There was an error retrieving your location" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+    [errorAlert show];
+    NSLog(@"Error: %@",error.description);
 }
 
+-(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    CLLocation *crnLoc = [locations lastObject];
+    latitudeData = [NSString stringWithFormat:@"%.8f",crnLoc.coordinate.latitude];
+    longitudeData = [NSString stringWithFormat:@"%.8f",crnLoc.coordinate.longitude];
+}
 @end
